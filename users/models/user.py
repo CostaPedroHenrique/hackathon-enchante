4c70395b7f71d2bd5aaf3e27b7c985264195b501
Pedro Costa <devpedrohenrique@gmail.com>
2022-03-18 19:47:51 -0300

    chore: inicialização do projeto

---
+++ users/models/user.py
@@ -0,0 +1,38 @@
+import re
+from django.db import models
+from django.core import validators
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(
+        'Apelido / Usuário', max_length=30, unique=True, validators=[
+            validators.RegexValidator(
+                re.compile('^[\w.@+-]+$'),
+                'Informe um nome de usuário válido. '
+                'Este valor deve conter apenas letras, números '
+                'e os caracteres: @/./+/-/_ .'
+                , 'invalid'
+            )
+        ], help_text='Um nome curto que será usado para identificá-lo de forma única na plataforma'
+    )
+    name = models.CharField('Nome Completo', max_length=100, blank=False)
+    email = models.EmailField('E-mail', unique=True)
+    birthDate = models.DateField('Data de nascimento', max_length=8, null=True, blank=True)
+    is_staff = models.BooleanField('Equipe', default=False)
+    is_active = models.BooleanField('Ativo', default=True)
+    date_joined = models.DateTimeField('Criado em', auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    class Meta:
+        app_label = 'users'
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name or self.username
