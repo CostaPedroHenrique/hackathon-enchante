0642dae8a417d834274b3a0b57881816f653bcde
Pedro Costa <devpedrohenrique@gmail.com>
2022-03-18 19:54:51 -0300

    chore: adicionado configurações do heroku

--- enchante/settings.py
+++ enchante/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from datetime import timedelta
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -157,3 +159,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
